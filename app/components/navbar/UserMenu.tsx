688cdd1af7dd63af7f750787e4399fdcbe668c4f
kimneighbor <designkimdh@gmail.com>
2023-09-07 01:32:13 +0900

    add UserMenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,15 @@
 
 import {AiOutlineMenu} from "react-icons/ai";
 import Avatar from "@/app/components/Avatar";
+import {useCallback, useState} from "react";
 
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -25,7 +32,9 @@ const UserMenu = () => {
                 >
                     Kimbnb your home
                 </div>
+
                 <div
+                    onClick={toggleOpen}
                     className="
                     p-4
                     md:py-1
@@ -45,11 +54,30 @@ const UserMenu = () => {
                     <AiOutlineMenu/>
                     <div className="hidden md:block">
                         <Avatar/>
-
                     </div>
                 </div>
             </div>
-
+            {isOpen && (
+                <div
+                    className="
+                    absolute
+                    rounded-xl
+                    shadow-md
+                    w-[40vw]
+                    md:w-3/4
+                    bg-white
+                    overflow-hidden
+                    right-0
+                    top-12
+                    text-sm
+                    "
+                >
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
