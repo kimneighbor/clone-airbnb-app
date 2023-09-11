2b5572ecb2e898265b6eeb31d7afa852b9149d05
kimneighbor <designkimdh@gmail.com>
2023-09-12 04:29:45 +0900

    update RegisterModal.tsx

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -12,6 +12,8 @@ import {
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "@/app/components/modals/Modal";
+import Heading from "@/app/components/Heading";
+import Input from "@/app/components/inputs/Input";
 
 const RegisterModal = () => {
         const registerModal = useRegisterModal();
@@ -36,9 +38,9 @@ const RegisterModal = () => {
             setIsLoading(true);
 
             axios.post('/api/register', data)
-            .then(() => {
-                registerModal.onClose();
-            })
+                .then(() => {
+                    registerModal.onClose();
+                })
                 .catch((error) => {
                     console.log(error);
                 })
@@ -47,18 +49,55 @@ const RegisterModal = () => {
                 })
         }
 
+        const bodyContent = (
+            <div className="flex flex-col gap-4">
+                <Heading
+                  title="Welcome to Kimbnb"
+                  subtitle="Create an account to get started!"
+                />
+                <Input
+                    id="email"
+                    label="Email"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+                <Input
+                    id="name"
+                    label="Name"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
 
-    return (
-        <Modal
-            disabled={isLoading}
-            isOpen={registerModal.isOpen}
-            title="Register"
-            actionLabel="Continue"
-            onClose={registerModal.onClose}
-            onSubmit={handleSubmit(onSubmit)}
-        />
+                <Input
+                    id="password"
+                    type="password"
+                    label="Password"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
 
-    );
+            </div>
+        )
+
+
+        return (
+            <Modal
+                disabled={isLoading}
+                isOpen={registerModal.isOpen}
+                title="Register"
+                actionLabel="Continue"
+                onClose={registerModal.onClose}
+                onSubmit={handleSubmit(onSubmit)}
+                body={bodyContent}
+            />
+
+        );
     }
 ;
 
