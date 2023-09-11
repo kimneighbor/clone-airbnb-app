dbeeec4d45dacbbcf120029aa80597aeaaafe176
kimneighbor <designkimdh@gmail.com>
2023-09-12 00:09:25 +0900

    update MenuItem

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,10 +1,30 @@
+'use client'
 
-const MenuItem = () => {
+import React from "react";
+
+// type interface
+interface MenuItemProps {
     onClick: () => void;
     label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({
+                                               // 사용할 type props
+                                               onClick,
+                                               label
+                                           }) => {
 
     return (
-        <div>
+        <div
+            onClick={onClick}
+            className="
+                px-4
+                py-3
+                hover:bg-neutral-100
+                transition
+                font-semibold
+                "
+        >
 
         </div>
     );
