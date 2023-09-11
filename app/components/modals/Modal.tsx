2914bd1da33ff6db010257fa43f93ab8d2fcd6d7
kimneighbor <designkimdh@gmail.com>
2023-09-12 03:11:34 +0900

    update Modal.tsx

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -2,6 +2,7 @@
 
 import React, {useCallback, useEffect, useState} from "react";
 import {IoMdClose} from "react-icons/io";
+import Button from "@/app/components/Button";
 
 interface ModalProps {
     isOpen?: boolean;
@@ -13,7 +14,7 @@ interface ModalProps {
     actionLabel: string;
     disabled?: boolean;
     secondaryAction?: () => void;
-    secondaryLabel?: string;
+    secondaryActionLabel?: string;
 }
 
 
@@ -27,7 +28,7 @@ const Modal: React.FC<ModalProps> = ({
                                          actionLabel,
                                          disabled,
                                          secondaryAction,
-                                         secondaryLabel
+                                         secondaryActionLabel
                                      }) => {
     const [showModal, setShowModal] = useState(isOpen)
 
@@ -168,6 +169,19 @@ const Modal: React.FC<ModalProps> = ({
                                     w-full
                                     "
                                 >
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button
+                                            outline
+                                            disabled={disabled}
+                                            label={secondaryActionLabel}
+                                            onClick={handleSecondaryAction}
+                                        />
+                                    )}
+                                    <Button
+                                        disabled={disabled}
+                                        label={actionLabel}
+                                        onClick={handleSubmit}
+                                    />
                                 </div>
                             </div>
                         </div>
