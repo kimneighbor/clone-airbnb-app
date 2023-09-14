257508cc85a425ac0a08476931b36b69d9e53a97
kimneighbor <designkimdh@gmail.com>
2023-09-14 20:15:02 +0900

    chore commit

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,14 +1,10 @@
-'use client';
+"use client";
 
 import axios from "axios";
-import {AiFillGithub} from "react-icons/ai";
-import {FcGoogle} from "react-icons/fc";
-import {useCallback, useState} from "react";
-import {
-    FieldValues,
-    SubmitHandler,
-    useForm
-} from "react-hook-form";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "@/app/components/modals/Modal";
@@ -18,134 +14,123 @@ import toast from "react-hot-toast";
 import Button from "@/app/components/Button";
 
 const RegisterModal = () => {
-        const registerModal = useRegisterModal();
-        const [isLoading, setIsLoading] = useState(false);
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-        const {
-            register,
-            handleSubmit,
-            formState: {
-                errors,
-            }
-        }
-            = useForm<FieldValues>({
-            defaultValues: {
-                name: "",
-                email: "",
-                password: ""
-            }
-        });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-        const onSubmit: SubmitHandler<FieldValues> = (data) => {
-            setIsLoading(true);
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
 
-            axios.post('/api/register', data)
-                .then(() => {
-                    registerModal.onClose();
-                })
-                .catch((error) => {
-                    toast.error("에러내용 : " + error.response.data.message);
-                })
-                .finally(() => {
-                    setIsLoading(false);
-                })
-        }
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        toast.error("에러내용 : " + error.response.data.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-        const bodyContent = (
-            <div className="flex flex-col gap-4">
-                <Heading
-                    title="Welcome to Kimbnb"
-                    subtitle="Create an account to get started!"
-                />
-                <Input
-                    id="email"
-                    label="Email"
-                    disabled={isLoading}
-                    register={register}
-                    errors={errors}
-                    required
-                />
-                <Input
-                    id="name"
-                    label="Name"
-                    disabled={isLoading}
-                    register={register}
-                    errors={errors}
-                    required
-                />
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome to Kimbnb"
+        subtitle="Create an account to get started!"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
 
-                <Input
-                    id="password"
-                    type="password"
-                    label="Password"
-                    disabled={isLoading}
-                    register={register}
-                    errors={errors}
-                    required
-                />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
 
-            </div>
-        );
-
-        const footerContent = (
-            <div className="flex flex-col gap-4 mt-3">
-                <hr/>
-                <Button
-                    outline
-                    label="Continue with Google"
-                    icon={FcGoogle}
-                    onClick={() => {
-                    }}
-                />
-                <Button
-                    outline
-                    label="Continue with Github"
-                    icon={AiFillGithub}
-                    onClick={() => {
-                    }}
-                />
-                <div
-                    className="
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
                     text-neutral-500
                     text-center
                     mt-4
                     font-light
                     "
-                >
-                    <div className="justify-center flex flex-row items-center gap-2">
-                        <div>
-                            Already have an account?
-                        </div>
-                        <div
-                            onClick={registerModal.onClose}
-                            className="
+      >
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="
                             text-neutral-800
                             cursor-pointer
                             hover:underline
                             "
-                        >
-                            Login
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
+          >
+            Login
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
-
-        return (
-            <Modal
-                disabled={isLoading}
-                isOpen={registerModal.isOpen}
-                title="Register"
-                actionLabel="Continue"
-                onClose={registerModal.onClose}
-                onSubmit={handleSubmit(onSubmit)}
-                body={bodyContent}
-                footer={footerContent}
-            />
-
-        );
-    }
-;
-
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
 export default RegisterModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,32 @@
-import './globals.css'
-import {Inter} from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 import Navbar from "@/app/components/navbar/Navbar";
 import ClientOnly from "@/app/components/ClientOnly";
 import RegisterModal from "@/app/components/modals/RegisterModal";
 import ToasterProvider from "@/app/providers/ToasterProvider";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Airbnb Clone',
-    description: 'Airbnb kim',
-}
-
+  title: "Airbnb Clone",
+  description: "Airbnb kim",
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>
+  return (
+    <html lang="en">
+      <body className={inter.className}>
         <ClientOnly>
-            <ToasterProvider/>
-            <RegisterModal/>
-            <Navbar/>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
         </ClientOnly>
         {children}
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
